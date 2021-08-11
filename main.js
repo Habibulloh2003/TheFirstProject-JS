8067b2119853092b91274eda9f4610bd87382e80
Khabibullokh <86731331+Habibulloh2003@users.noreply.github.com>
2021-08-12 01:41:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var yil = prompt("Tug'ilgan yilingizni kiriting")
+var user_name = prompt('ismingizni kiriting');
+var user_age = prompt('Yoshingizni kiriting');
+var misol1 = prompt('Misolni yeching 6 + 3 = ?');
+var misol2 = prompt('Misolni yeching 20 - 5 = ?');
+var misol3 = prompt('Misolni yeching 15 * 2 = ?');
+var misol4 = prompt('Misolni yeching 8 / 4 = ?');
+var misol5 = prompt('Misolni yeching 10 % 3 = ?');
+prompt("Son o'ylang");
+prompt("Unga 10 ni qo'shing");
+prompt("Javobdan 5 ni ayiring");
+prompt("Javobni 10 ga ko'paytiring");
+prompt("Javobni 5 ga ko'paytiring");
+prompt("Javobni 50 ga bo'ling");
+prompt("Javobdan o'ylagan soningizni ayiring");
+
+
+console.log('Sizning yoshingiz ' + (2021 - yil) + ' da');
+console.log('Sizning ismingiz ' + user_name);
+console.log('Siz ' + user_age + ' yoshdasiz');
+console.log('Misol 1: 6 + 3 = 9 Sizning javob ' + misol1);
+console.log('Misol 2: 20 - 5 = 15 Sizning javob ' + misol2);
+console.log('Misol 3: 15 * 2 = 30 Sizning javob ' + misol3);
+console.log('Misol 4: 8 / 4 = 2 Sizning javob ' + misol4);
+console.log('Misol 5: 10 % 3 = 1 Sizning javob ' + misol5);
+console.log('Chiqgan javob 5');
